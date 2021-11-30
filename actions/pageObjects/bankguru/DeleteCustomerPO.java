2dfcc35411b8339dae7c158e2b327b5653f8bde8
quanh <quanh@DESKTOP-05J6JRM>
2021-11-30 23:29:54 +0700

    live coding

---
+++ actions/pageObjects/bankguru/DeleteCustomerPO.java
@@ -0,0 +1,23 @@
+package pageObjects.bankguru;
+
+import org.openqa.selenium.WebDriver;
+
+import commons.BasePage;
+import pageUIs.bankguru.BasePageUI;
+import pageUIs.bankguru.DepositUI;
+import pageUIs.bankguru.EditAccountUI;
+import pageUIs.bankguru.HomePageUI;
+import pageUIs.bankguru.NewCustomerUI;
+
+public class DeleteCustomerPO extends BasePage {
+	private WebDriver driver;
+	
+	public DeleteCustomerPO(WebDriver driver) {
+		this.driver=driver;
+	}
+
+	
+
+	LoginPO loginPage;
+}
+	
